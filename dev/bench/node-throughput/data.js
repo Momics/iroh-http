aaa2cd05ccbab36776f35431453cc8d7d68506af
github-action-benchmark <github@users.noreply.github.com>
2026-04-29 07:40:34 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 62b6b236cb60ee4f069b1507ee26e6fdb6eabd11

--- dev/bench/node-throughput/data.js
+++ dev/bench/node-throughput/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777222716672,
+  "lastUpdate": 1777448434404,
   "repoUrl": "https://github.com/Momics/iroh-http",
   "entries": {
     "Benchmark": [
@@ -61,6 +61,68 @@ window.BENCHMARK_DATA = {
           {
             "name": "node/native/throughput-10mb",
             "value": 588.7635951548941,
+            "unit": "MB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Momics",
+            "username": "Momics",
+            "email": "momics.eu@gmail.com"
+          },
+          "committer": {
+            "name": "Momics",
+            "username": "Momics",
+            "email": "momics.eu@gmail.com"
+          },
+          "id": "62b6b236cb60ee4f069b1507ee26e6fdb6eabd11",
+          "message": "fix: update fetch URLs to use dynamic serverId in benchmark scripts",
+          "timestamp": "2026-04-29T07:35:37Z",
+          "url": "https://github.com/Momics/iroh-http/commit/62b6b236cb60ee4f069b1507ee26e6fdb6eabd11"
+        },
+        "date": 1777448434110,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "node/iroh/throughput-1kb",
+            "value": 1.5435551206586888,
+            "unit": "MB/s"
+          },
+          {
+            "name": "node/native/throughput-1kb",
+            "value": 2.010453152048877,
+            "unit": "MB/s"
+          },
+          {
+            "name": "node/iroh/throughput-64kb",
+            "value": 57.38305855998946,
+            "unit": "MB/s"
+          },
+          {
+            "name": "node/native/throughput-64kb",
+            "value": 78.26883071676703,
+            "unit": "MB/s"
+          },
+          {
+            "name": "node/iroh/throughput-1mb",
+            "value": 166.58976771928909,
+            "unit": "MB/s"
+          },
+          {
+            "name": "node/native/throughput-1mb",
+            "value": 432.9749270800958,
+            "unit": "MB/s"
+          },
+          {
+            "name": "node/iroh/throughput-10mb",
+            "value": 217.2296689457865,
+            "unit": "MB/s"
+          },
+          {
+            "name": "node/native/throughput-10mb",
+            "value": 666.058661677942,
             "unit": "MB/s"
           }
         ]
