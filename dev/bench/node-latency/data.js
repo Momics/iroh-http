ed79ed74b1e1b590eb949c7eb9b4aeaca7a90b5f
github-action-benchmark <github@users.noreply.github.com>
2026-04-29 07:40:34 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 62b6b236cb60ee4f069b1507ee26e6fdb6eabd11

--- dev/bench/node-latency/data.js
+++ dev/bench/node-latency/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777222717082,
+  "lastUpdate": 1777448434793,
   "repoUrl": "https://github.com/Momics/iroh-http",
   "entries": {
     "Benchmark": [
@@ -71,6 +71,78 @@ window.BENCHMARK_DATA = {
           {
             "name": "node/native/serve-rps",
             "value": 241.72811999997066,
+            "unit": "us"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Momics",
+            "username": "Momics",
+            "email": "momics.eu@gmail.com"
+          },
+          "committer": {
+            "name": "Momics",
+            "username": "Momics",
+            "email": "momics.eu@gmail.com"
+          },
+          "id": "62b6b236cb60ee4f069b1507ee26e6fdb6eabd11",
+          "message": "fix: update fetch URLs to use dynamic serverId in benchmark scripts",
+          "timestamp": "2026-04-29T07:35:37Z",
+          "url": "https://github.com/Momics/iroh-http/commit/62b6b236cb60ee4f069b1507ee26e6fdb6eabd11"
+        },
+        "date": 1777448434760,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "node/iroh/cold-connect",
+            "value": 101234.1056,
+            "unit": "us"
+          },
+          {
+            "name": "node/native/cold-connect",
+            "value": 1442.2863000000461,
+            "unit": "us"
+          },
+          {
+            "name": "node/iroh/warm-request",
+            "value": 700.4187600000205,
+            "unit": "us"
+          },
+          {
+            "name": "node/native/warm-request",
+            "value": 852.5855599999886,
+            "unit": "us"
+          },
+          {
+            "name": "node/iroh/multiplex-x8",
+            "value": 2502.2579600000063,
+            "unit": "us"
+          },
+          {
+            "name": "node/native/multiplex-x8",
+            "value": 2462.4487999999474,
+            "unit": "us"
+          },
+          {
+            "name": "node/iroh/multiplex-x32",
+            "value": 6845.145359999933,
+            "unit": "us"
+          },
+          {
+            "name": "node/native/multiplex-x32",
+            "value": 5243.365319999939,
+            "unit": "us"
+          },
+          {
+            "name": "node/iroh/serve-rps",
+            "value": 510.7450000000062,
+            "unit": "us"
+          },
+          {
+            "name": "node/native/serve-rps",
+            "value": 227.39304000002448,
             "unit": "us"
           }
         ]
