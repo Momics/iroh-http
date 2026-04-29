a6b34d80ce9d1399338043c95947678d6001a9bb
github-action-benchmark <github@users.noreply.github.com>
2026-04-29 14:43:45 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for c1bd55054f9a90012dfb0fe901771a7da994726b

---
+++ dev/bench/releases/node-throughput/data.js
@@ -0,0 +1,72 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1777473825340,
+  "repoUrl": "https://github.com/Momics/iroh-http",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "momics.eu@gmail.com",
+            "name": "Momics",
+            "username": "Momics"
+          },
+          "committer": {
+            "email": "momics.eu@gmail.com",
+            "name": "Momics",
+            "username": "Momics"
+          },
+          "distinct": true,
+          "id": "c1bd55054f9a90012dfb0fe901771a7da994726b",
+          "message": "chore: release v0.3.4",
+          "timestamp": "2026-04-29T16:39:45+02:00",
+          "tree_id": "5245e1f8ed6632a04638e1ae8b231b38b16e4c1e",
+          "url": "https://github.com/Momics/iroh-http/commit/c1bd55054f9a90012dfb0fe901771a7da994726b"
+        },
+        "date": 1777473824600,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "node/iroh/throughput-1kb",
+            "value": 1.645974337590428,
+            "unit": "MB/s"
+          },
+          {
+            "name": "node/native/throughput-1kb",
+            "value": 2.126612703303391,
+            "unit": "MB/s"
+          },
+          {
+            "name": "node/iroh/throughput-64kb",
+            "value": 68.03663048642194,
+            "unit": "MB/s"
+          },
+          {
+            "name": "node/native/throughput-64kb",
+            "value": 91.35463577272101,
+            "unit": "MB/s"
+          },
+          {
+            "name": "node/iroh/throughput-1mb",
+            "value": 175.17767272832137,
+            "unit": "MB/s"
+          },
+          {
+            "name": "node/native/throughput-1mb",
+            "value": 435.30160934139104,
+            "unit": "MB/s"
+          },
+          {
+            "name": "node/iroh/throughput-10mb",
+            "value": 233.3305118230073,
+            "unit": "MB/s"
+          },
+          {
+            "name": "node/native/throughput-10mb",
+            "value": 682.6567915537651,
+            "unit": "MB/s"
+          }
+        ]
+      }
+    ]
+  }
+}
