5a0f1a4e7035fdd4d161f44aea09afa25d1aace0
github-action-benchmark <github@users.noreply.github.com>
2026-05-02 08:45:05 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 61efb1af312c759c354ff9e1b1d2f6b15eca32c2

--- dev/bench/releases/deno-latency/data.js
+++ dev/bench/releases/deno-latency/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777473766905,
+  "lastUpdate": 1777711505726,
   "repoUrl": "https://github.com/Momics/iroh-http",
   "entries": {
     "Benchmark": [
@@ -73,6 +73,80 @@ window.BENCHMARK_DATA = {
           {
             "name": "deno/native/serve-rps",
             "value": 623.3222000000387,
+            "unit": "us"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "momics.eu@gmail.com",
+            "name": "Momics",
+            "username": "Momics"
+          },
+          "committer": {
+            "email": "momics.eu@gmail.com",
+            "name": "Momics",
+            "username": "Momics"
+          },
+          "distinct": true,
+          "id": "61efb1af312c759c354ff9e1b1d2f6b15eca32c2",
+          "message": "chore: release v0.4.0",
+          "timestamp": "2026-05-02T10:41:19+02:00",
+          "tree_id": "7850aebbdf11874a3ef87962e8195507aa7fbc1d",
+          "url": "https://github.com/Momics/iroh-http/commit/61efb1af312c759c354ff9e1b1d2f6b15eca32c2"
+        },
+        "date": 1777711505688,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "deno/iroh/cold-connect",
+            "value": 1016886.3091999999,
+            "unit": "us"
+          },
+          {
+            "name": "deno/native/cold-connect",
+            "value": 689.5326000003479,
+            "unit": "us"
+          },
+          {
+            "name": "deno/iroh/warm-request",
+            "value": 1064.653199999957,
+            "unit": "us"
+          },
+          {
+            "name": "deno/native/warm-request",
+            "value": 601.144520000089,
+            "unit": "us"
+          },
+          {
+            "name": "deno/iroh/multiplex-x8",
+            "value": 3116.25256000043,
+            "unit": "us"
+          },
+          {
+            "name": "deno/native/multiplex-x8",
+            "value": 1074.1594800000166,
+            "unit": "us"
+          },
+          {
+            "name": "deno/iroh/multiplex-x32",
+            "value": 8962.399440000008,
+            "unit": "us"
+          },
+          {
+            "name": "deno/native/multiplex-x32",
+            "value": 2826.845880000328,
+            "unit": "us"
+          },
+          {
+            "name": "deno/iroh/serve-rps",
+            "value": 633.0675600002724,
+            "unit": "us"
+          },
+          {
+            "name": "deno/native/serve-rps",
+            "value": 530.2608000003238,
             "unit": "us"
           }
         ]
