26ab18e0b1f7cacae443b944901b00c603ead887
github-action-benchmark <github@users.noreply.github.com>
2026-05-02 08:47:58 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 61efb1af312c759c354ff9e1b1d2f6b15eca32c2

--- dev/bench/releases/rust-latency/data.js
+++ dev/bench/releases/rust-latency/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777473994746,
+  "lastUpdate": 1777711678147,
   "repoUrl": "https://github.com/Momics/iroh-http",
   "entries": {
     "Benchmark": [
@@ -58,6 +58,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "rust/multiplex/8",
             "value": 1303.2134137929463,
+            "unit": "us"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "momics.eu@gmail.com",
+            "name": "Momics",
+            "username": "Momics"
+          },
+          "committer": {
+            "email": "momics.eu@gmail.com",
+            "name": "Momics",
+            "username": "Momics"
+          },
+          "distinct": true,
+          "id": "61efb1af312c759c354ff9e1b1d2f6b15eca32c2",
+          "message": "chore: release v0.4.0",
+          "timestamp": "2026-05-02T10:41:19+02:00",
+          "tree_id": "7850aebbdf11874a3ef87962e8195507aa7fbc1d",
+          "url": "https://github.com/Momics/iroh-http/commit/61efb1af312c759c354ff9e1b1d2f6b15eca32c2"
+        },
+        "date": 1777711678093,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "rust/connection_establishment",
+            "value": 5106.709571999998,
+            "unit": "us"
+          },
+          {
+            "name": "rust/fetch_get_latency",
+            "value": 251.663487325593,
+            "unit": "us"
+          },
+          {
+            "name": "rust/handle_ops/alloc_body_writer",
+            "value": 0.22340752556231047,
+            "unit": "us"
+          },
+          {
+            "name": "rust/handle_ops/make_body_channel",
+            "value": 0.18320271559258536,
+            "unit": "us"
+          },
+          {
+            "name": "rust/latency_iroh_1kb",
+            "value": 257.5188157036147,
+            "unit": "us"
+          },
+          {
+            "name": "rust/multiplex/32",
+            "value": 2272.0373073913047,
+            "unit": "us"
+          },
+          {
+            "name": "rust/multiplex/8",
+            "value": 769.9318806927048,
             "unit": "us"
           }
         ]
