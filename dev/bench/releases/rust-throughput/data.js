1a7074709b8abf887f651ad24f806ddf50b73163
github-action-benchmark <github@users.noreply.github.com>
2026-05-02 08:47:57 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 61efb1af312c759c354ff9e1b1d2f6b15eca32c2

--- dev/bench/releases/rust-throughput/data.js
+++ dev/bench/releases/rust-throughput/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777473994506,
+  "lastUpdate": 1777711677911,
   "repoUrl": "https://github.com/Momics/iroh-http",
   "entries": {
     "Benchmark": [
@@ -63,6 +63,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "rust/throughput_response_body/65536",
             "value": 101.22059935995125,
+            "unit": "MB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "momics.eu@gmail.com",
+            "name": "Momics",
+            "username": "Momics"
+          },
+          "committer": {
+            "email": "momics.eu@gmail.com",
+            "name": "Momics",
+            "username": "Momics"
+          },
+          "distinct": true,
+          "id": "61efb1af312c759c354ff9e1b1d2f6b15eca32c2",
+          "message": "chore: release v0.4.0",
+          "timestamp": "2026-05-02T10:41:19+02:00",
+          "tree_id": "7850aebbdf11874a3ef87962e8195507aa7fbc1d",
+          "url": "https://github.com/Momics/iroh-http/commit/61efb1af312c759c354ff9e1b1d2f6b15eca32c2"
+        },
+        "date": 1777711676950,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "rust/throughput_post_body/1024",
+            "value": 3.599877777680985,
+            "unit": "MB/s"
+          },
+          {
+            "name": "rust/throughput_post_body/1048576",
+            "value": 314.33386138141185,
+            "unit": "MB/s"
+          },
+          {
+            "name": "rust/throughput_post_body/10485760",
+            "value": 336.0649216678206,
+            "unit": "MB/s"
+          },
+          {
+            "name": "rust/throughput_post_body/65536",
+            "value": 137.6507344261649,
+            "unit": "MB/s"
+          },
+          {
+            "name": "rust/throughput_response_body/1024",
+            "value": 3.955984571412043,
+            "unit": "MB/s"
+          },
+          {
+            "name": "rust/throughput_response_body/1048576",
+            "value": 327.2062058101451,
+            "unit": "MB/s"
+          },
+          {
+            "name": "rust/throughput_response_body/10485760",
+            "value": 347.7682543708502,
+            "unit": "MB/s"
+          },
+          {
+            "name": "rust/throughput_response_body/65536",
+            "value": 149.93947112829247,
             "unit": "MB/s"
           }
         ]
