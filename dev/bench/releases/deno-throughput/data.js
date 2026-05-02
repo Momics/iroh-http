99995b94eec61b4e8ae33291f964808fc7c6240d
github-action-benchmark <github@users.noreply.github.com>
2026-05-02 08:45:05 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 61efb1af312c759c354ff9e1b1d2f6b15eca32c2

--- dev/bench/releases/deno-throughput/data.js
+++ dev/bench/releases/deno-throughput/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777473766679,
+  "lastUpdate": 1777711505504,
   "repoUrl": "https://github.com/Momics/iroh-http",
   "entries": {
     "Benchmark": [
@@ -63,6 +63,70 @@ window.BENCHMARK_DATA = {
           {
             "name": "deno/native/throughput-10mb",
             "value": 833.9738739169825,
+            "unit": "MB/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "momics.eu@gmail.com",
+            "name": "Momics",
+            "username": "Momics"
+          },
+          "committer": {
+            "email": "momics.eu@gmail.com",
+            "name": "Momics",
+            "username": "Momics"
+          },
+          "distinct": true,
+          "id": "61efb1af312c759c354ff9e1b1d2f6b15eca32c2",
+          "message": "chore: release v0.4.0",
+          "timestamp": "2026-05-02T10:41:19+02:00",
+          "tree_id": "7850aebbdf11874a3ef87962e8195507aa7fbc1d",
+          "url": "https://github.com/Momics/iroh-http/commit/61efb1af312c759c354ff9e1b1d2f6b15eca32c2"
+        },
+        "date": 1777711505190,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "deno/iroh/throughput-1kb",
+            "value": 1.2262359629767419,
+            "unit": "MB/s"
+          },
+          {
+            "name": "deno/native/throughput-1kb",
+            "value": 1.5759360525592678,
+            "unit": "MB/s"
+          },
+          {
+            "name": "deno/iroh/throughput-64kb",
+            "value": 39.25438820723139,
+            "unit": "MB/s"
+          },
+          {
+            "name": "deno/native/throughput-64kb",
+            "value": 95.19566807009062,
+            "unit": "MB/s"
+          },
+          {
+            "name": "deno/iroh/throughput-1mb",
+            "value": 66.16803126453392,
+            "unit": "MB/s"
+          },
+          {
+            "name": "deno/native/throughput-1mb",
+            "value": 528.5918056345654,
+            "unit": "MB/s"
+          },
+          {
+            "name": "deno/iroh/throughput-10mb",
+            "value": 77.07431665309696,
+            "unit": "MB/s"
+          },
+          {
+            "name": "deno/native/throughput-10mb",
+            "value": 929.9857269510547,
             "unit": "MB/s"
           }
         ]
